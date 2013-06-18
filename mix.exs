6fb81c56d5d11a5a8eb6126e6016bcfd64606ecf
meh <meh@schizofreni.co>
2013-06-18 20:55:55 +0200

    mix: remove benchmark dependency

--- mix.exs
+++ mix.exs
@@ -16,7 +16,6 @@ defmodule Cauldron.Mixfile do
   # { :foobar, "0.1", git: "https://github.com/elixir-lang/foobar.git" }
   defp deps do
     [ { :datastructures, %r".*", github: "meh/elixir-datastructures" },
-      { :socket, %r".*", github: "meh/elixir-socket" },
-      { :benchmark, %r".*", github: "meh/elixir-benchmark" } ]
+      { :socket, %r".*", github: "meh/elixir-socket" } ]
   end
 end
