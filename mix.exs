834e0818d498a3fc8527117ad8c6c0b9a7adabcb
meh <meh@schizofreni.co>
2013-11-03 13:34:50 +0100

    mix: bump Elixir version requirement

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Cauldron.Mixfile do
   def project do
     [ app: :cauldron,
       version: "0.0.1",
-      elixir: "~> 0.10.4-dev",
+      elixir: "~> 0.11.0",
       deps: deps ]
   end
 
