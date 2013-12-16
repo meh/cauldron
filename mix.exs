751672a776fb4c9c09d32065058a4389c76370aa
meh <meh@schizofreni.co>
2013-12-16 12:55:57 +0100

    mix: update Elixir version requirement

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Cauldron.Mixfile do
   def project do
     [ app: :cauldron,
       version: "0.0.1",
-      elixir: "~> 0.11.0",
+      elixir: "~> 0.11.0 or ~> 0.12.0",
       deps: deps ]
   end
 
