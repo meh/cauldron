8ea35aee070d2ef87c9f0f754eff152699aacea0
meh <meh@schizofreni.co>
2014-02-09 21:18:04 +0100

    mix: remove derp from deps

--- mix.exs
+++ mix.exs
@@ -10,14 +10,13 @@ defmodule Cauldron.Mixfile do
 
   # Configuration for the OTP application
   def application do
-    [ applications: [:socket, :derp] ]
+    [ applications: [:socket] ]
   end
 
   # Returns the list of dependencies in the format:
   # { :foobar, "0.1", git: "https://github.com/elixir-lang/foobar.git" }
   defp deps do
     [ { :datastructures, github: "meh/elixir-datastructures" },
-      { :reagent, github: "meh/reagent" },
-      { :derp, github: "meh/derp" } ]
+      { :reagent, github: "meh/reagent" } ]
   end
 end
