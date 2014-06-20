1027faae669a3a4a76095ca8e9e7b69b68515ae1
meh <meh@schizofreni.co>
2014-06-20 09:24:24 +0200

    mix: bump version and Elixir requirement

--- mix.exs
+++ mix.exs
@@ -3,8 +3,8 @@ defmodule Cauldron.Mixfile do
 
   def project do
     [ app: :cauldron,
-      version: "0.1.0",
-      elixir: "~> 0.13.2",
+      version: "0.1.1",
+      elixir: "~> 0.14.1",
       deps: deps,
       package: package,
       description: "Web server library written in Elixir" ]
@@ -12,15 +12,16 @@ defmodule Cauldron.Mixfile do
 
   defp package do
     [ contributors: ["meh"],
-      license: "WTFPL",
+      licenses: ["WTFPL"],
       links: [ { "GitHub", "https://github.com/meh/cauldron" } ] ]
   end
 
   def application do
-    [ applications: [:reagent] ]
+    [ applications: [:reagent, :httprot] ]
   end
 
   defp deps do
-    [ { :reagent, "~> 0.1.1" } ]
+    [ { :reagent, "~> 0.1.2" },
+      { :httprot, "~> 0.1.1" } ]
   end
 end
