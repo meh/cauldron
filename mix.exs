8c7f50fb9e9bb372d0108c3e4b06952cc3dfd27e
meh <meh@schizofreni.co>
2017-03-27 20:44:15 +0200

    mix: bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Cauldron.Mixfile do
 
   def project do
     [ app: :cauldron,
-      version: "0.1.6",
+      version: "0.1.7",
       deps: deps(),
       package: package(),
       description: "Web server library written in Elixir" ]
