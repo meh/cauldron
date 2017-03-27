862e989ce6be7088151defb82af9fbcceee42946
meh <meh@schizofreni.co>
2017-03-27 20:44:50 +0200

    mix: fix maintainers field

--- mix.exs
+++ mix.exs
@@ -10,7 +10,7 @@ defmodule Cauldron.Mixfile do
   end
 
   defp package do
-    [ contributors: ["meh"],
+    [ maintainers: ["meh"],
       licenses: ["WTFPL"],
       links: %{"GitHub" => "https://github.com/meh/cauldron"} ]
   end
